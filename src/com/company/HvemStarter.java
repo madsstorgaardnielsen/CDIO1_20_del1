4b34bd2647c005831168ed080d74a9c873e74cd8
Mads Storgaard-Nielsen <s180076@student.dtu.dk>
2019-09-30 19:06:31 +0200

    flere tilføjelser

--- src/com/company/HvemStarter.java
+++ src/com/company/HvemStarter.java
@@ -4,7 +4,7 @@ class HvemStarter {
     TerningeKast spiller1slag = new TerningeKast();
     TerningeKast spiller2slag = new TerningeKast();
 
-    public int kør() {
+    public int førsteKast() {
         int tur = 2;
         int startslag1;
         int startslag2;
@@ -12,19 +12,19 @@ class HvemStarter {
         do {
             startslag1 = spiller1slag.getØjne();
             startslag2 = spiller2slag.getØjne();
-            System.out.println("spiller 1 slog: " + startslag1 + "\nspiller 2 slog: " + startslag2);
+            System.out.println("Spiller 1 slog: " + startslag1 + "\nSpiller 2 slog: " + startslag2);
             //udskriver omslag inden nyt rul
             if (startslag1 == startslag2) {
-                System.out.println("omslag!");
+                System.out.println("Omslag!");
             }
         } while (startslag1 == startslag2);
 
         //giver turen til det højeste slag
         if (startslag1 > startslag2) {
-            System.out.println("spiller 1 starter!");
+            System.out.println("Spiller 1 starter!");
         } else {
             tur++;
-            System.out.println("spiller 2 starter!");
+            System.out.println("Spiller 2 starter!");
         }
 
         return tur;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,96 +2,132 @@ package com.company;
 
 import java.util.Scanner;
 
-public class  Main {
+public class Main {
     public static void main(String[] args) {
         int spiller1point = 0, spiller2point = 0;
-        int tur=0;
+
+        //tur variablen holder styr på hvis tur det er, hvis den er lige er det spiller1's tur, hvis ulige er det spiller2's tur
+        int tur = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Velkommen til terningspillet");
-        System.out.println("- et terning spil for to spillere, regler er følgende: første spiller til at ramme 40 point vinder!");
-        System.out.println("- Tast enter for at slå med terningerne");
+        System.out.println("- Et terning spil for to spillere, regler er følgende: første spiller til at ramme 40 point vinder!");
+        System.out.println("- Tryk på enter for at slå med terningerne");
         System.out.println("- God fornøjelse!\n\n");
 
+        //indtast navnene på de to spillere, de bliver gemt i variablerne spiller1 og spiller2
+        System.out.print("Indtast navnet på Spiller 1: ");
+        String spiller1 = sc.next().toString();
+        System.out.print("Indtast navnet på Spiller 2: ");
+        String spiller2 = sc.next().toString();
 
-        //indtast de to spilleres navne
-        System.out.print("Indtast navnet på spiller 1: ");
-        String spiller1=sc.next().toString();
-        System.out.print("Indtast navnet på spiller 2: ");
-        String spiller2=sc.next().toString();
+        System.out.print("\n---I skal slå om hvem der starter, tast enter for at slå---");
 
-        System.out.print("\n--- skal slå om hvem der starter, tast enter for at slå---");
-
-        //hokuspokus fra google der gør at der bliver ventet på enter bliver trykket før den ruller terningerne
+        //Venter på tasteinput fra brugeren inden der bliver slået om hvem der starter spillet.
         try {
             System.in.read();
         } catch (Exception e) {
         }
 
-        //Finder ud af hvem der starter spillet vha. HvemStarter metoden
-        HvemStarter starter = new HvemStarter();
-        tur = starter.kør();
-        System.out.println("Værdier af tur: "+tur+" hvis 2 = player1, 3 = player2");
+        //Der bliver slået om hvilken spiller der starter vha. førsteKast i HvemStarter klassen
+        //turene går på skift, spiller1 har de lige ture, spiller2 har de ulige ture.
+        HvemStarter startKast = new HvemStarter();
 
+        //sætter tur til hhv. 2 eller 3 vha. førsteKast metoden.
+        tur = startKast.førsteKast();
 
-        //Løkke spillet starter og kører i et loop indtil nogen rammer 40 point.
+        //Et loop der kører indtil en af spillerne rammer 40 point.
         //spiller 1's tur.
         while (true) {
             if (tur % 2 == 0) {
 
-                //kaster 2 terninger for spiller 1
+                //opretter to nye terningekast.
                 TerningeKast spiller1slag1 = new TerningeKast();
                 TerningeKast spiller1slag2 = new TerningeKast();
 
-                //hokuspokus fra google der gør at der bliver ventet på enter bliver trykket før den ruller terningerne
+                //Venter på tasteinput fra brugeren inden der bliver slået om hvem der starter spillet.
                 try {
                     System.in.read();
                 } catch (Exception e) {
                 }
 
+                //gemmer terningekastene i hhv. slag1 og slag2.
                 int slag1 = spiller1slag1.getØjne();
                 int slag2 = spiller1slag2.getØjne();
 
-                //lægger summen til point
-                int sum1 = slag1+slag2;
-                spiller1point = sum1+spiller1point;
 
-                System.out.println(spiller1 + " slår " + slag1 + " og " + slag2 + " hvilket svarer til "+sum1);
-                System.out.println(spiller1 + " har nu: " + spiller1point+" point!\n");
-                tur++;
+                //ligger de to slag sammen, og ligger dem så til spiller1's point
+                int sum1 = slag1 + slag2;
+                spiller1point = sum1 + spiller1point;
 
-                if (spiller1point >= 40) {
+                //Hvis der bliver slået to 1'ere bliver spiller2's point sat til 0.
+                if (slag1 == 1 && slag2 == 1) {
+                    spiller1point = 0;
+                }
+
+                //Udskriver spiller1's slag og spiller1's samlede point
+                System.out.println(spiller1 + " slår " + slag1 + " og " + slag2);
+                System.out.println(spiller1 + " har nu: " + spiller1point + " point!\n");
+
+                //hvis de 2 slag er ens bliver tur ikke sat en op og spiller1 får en ekstra tur
+                if (slag1 == slag2) {
+                    tur = tur;
+                    System.out.println("Ekstratur!");
+                } else
+                    tur++;
+
+                //hvis spiller1 rammer 40 point eller derover bliver løkken afbrudt
+                if (spiller1point >= 1000) {
                     break;
                 }
 
+                
                 //spiller 2's tur
-            } if (tur%2!=0) {
+            }
+            if (tur % 2 != 0) {
 
                 //opretter terninger til spiller 2
                 TerningeKast spiller2slag1 = new TerningeKast();
                 TerningeKast spiller2slag2 = new TerningeKast();
 
-                //hokuspokus fra google der gør at der bliver ventet på enter bliver trykket før den ruller terningerne
+                //Venter på tasteinput fra brugeren inden der bliver slået om hvem der starter spillet.
                 try {
                     System.in.read();
                 } catch (Exception e) {
                 }
 
+                //gemmer terningekastene i hhv. slag1 og slag2.
                 int slag1 = spiller2slag1.getØjne();
                 int slag2 = spiller2slag2.getØjne();
 
-                int sum2 = slag1+slag2;
-                spiller2point = sum2 +spiller2point;
+                //ligger de to slag sammen, og ligger dem så til spiller2's point
+                int sum2 = slag1 + slag2;
+                spiller2point = sum2 + spiller2point;
 
-                System.out.println(spiller2 + " slår " + slag1 + " og " + slag2 + " hvilket svarer til: " + sum2 + " point!");
-                System.out.println(spiller2 + " har nu: " + spiller2point+" point!\n");
-                tur++;
+                //Hvis der bliver slået to 1'ere bliver spiller2's point sat til 0.
+                if (slag1 == 1 && slag2 == 1) {
+                    spiller2point = 0;
+                }
 
-                if (spiller2point >= 40) {
+                //Udskriver spiller1's slag og spiller1's samlede point
+                System.out.println(spiller2 + " slår " + slag1 + " og " + slag2);
+                System.out.println(spiller2 + " har nu: " + spiller2point + " point!\n");
+
+                //hvis de 2 slag er ens bliver tur ikke sat en op og spiller2 får en ekstra tur
+                if (slag1 == slag2) {
+                    tur = tur;
+                    System.out.println("Ekstratur!");
+                } else
+                    tur++;
+
+
+                //hvis spiller2 rammer 40 point eller derover bliver løkken afbrudt
+                if (spiller2point >= 1000) {
                     break;
                 }
             }
         }
-        //udskriver vinderen og hvor mange point han/hun/hen har og taberens point
+
+        //udskriver vinderen og hvor mange point personen har og taberens point
         if (spiller1point > spiller2point) {
             System.out.println("\n\n\n\n" + spiller1 + " vinder med " + spiller1point + " point mod " + spiller2 + "'s " + spiller2point + " point");
         } else {
